a1add0c02066d7389cab0b11c6c6c1f09de1a22e
Angela Zhu <anz047@ucsd.edu>
2019-02-28 00:00:07 -0800

    new

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
-}
+  L.mapquest.key = '9h0ar19BA3eAf08zGLNdABdW8YRB4F3h';
+
+  	var map = L.mapquest.map('map', {
+  		center: [32.879353, -117.2311],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12,
+  		zoomControl: false
+  	});
+
+  	L.marker([32.88, -117.236]).addTo(map);
+  }
